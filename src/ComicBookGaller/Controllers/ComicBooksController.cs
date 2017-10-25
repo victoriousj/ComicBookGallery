adb6577232c4aa04fdda4265f3e2ad560a4eceb1
Victor Johnson <victordjohnson@gmail.com>
2017-10-24 19:53:25 -0500

    Playing with the viewbag.

--- src/ComicBookGaller/Controllers/ComicBooksController.cs
+++ src/ComicBookGaller/Controllers/ComicBooksController.cs
@@ -10,14 +10,19 @@ namespace ComicBookGaller.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
             {
-                return Redirect("/");
-            }
-            return new ContentResult()
-            {
-                Content = "Hello from the comic books controller",
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
             };
+
+            return View();
         }
     }
 }
