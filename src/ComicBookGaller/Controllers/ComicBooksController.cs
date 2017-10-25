0c1ced680dbffab4f8f606bc359e0f2df82eab4a
Victor Johnson <victordjohnson@gmail.com>
2017-10-24 19:29:34 -0500

    Updated detail Action Method to use the new methods for the detiail page of the comic book controller mthoed

--- src/ComicBookGaller/Controllers/ComicBooksController.cs
+++ src/ComicBookGaller/Controllers/ComicBooksController.cs
@@ -8,5 +8,16 @@ namespace ComicBookGaller.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+            return new ContentResult()
+            {
+                Content = "Hello from the comic books controller",
+            };
+        }
     }
 }
